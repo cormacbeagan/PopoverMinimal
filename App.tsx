5b4516a740ee53574589000fbd312974b12b5772
cormacbeagan <cormacbeagan@gmail.com>
2023-01-02 13:42:37 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {enableScreens} from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import Popover from 'react-native-popover-view/dist/Popover';
+enableScreens();
+const Stack = createNativeStackNavigator();
+
+const Screen1 = ({navigation}) => {
+  const onPress = () => {
+    navigation.navigate('Screen2');
+  };
+  return (
+    <View style={styles.screenView}>
+      <Text style={styles.text}>Screen 1</Text>
+      <Popover debug from={<Button title="Popover" />}>
+        <Button title="Screen2" onPress={onPress} />
+      </Popover>
+    </View>
+  );
+};
+
+const Screen2 = ({navigation}) => {
+  const onPress = () => {
+    navigation.navigate('Screen1');
+  };
+  return (
+    <View style={styles.screenView}>
+      <Text style={styles.text}>Screen 2 </Text>
+      <Popover debug from={<Button title="Popover" />}>
+        <Button title="Screen1" onPress={onPress} />
+      </Popover>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  screenView: {
+    height: '100%',
+    width: '100%',
+  },
+});
+
+export default App;
